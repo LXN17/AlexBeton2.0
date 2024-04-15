c7a30460bcf09fed14fe69452752bf6b28376888
LXN <leqso_leqso.05@mail.ru>
2024-04-16 01:28:06 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Contact from './components/main/Contact'
+import Price from './components/main/Price'
+import Privelege from './components/main/Privelege'
+import Vehicle from './components/main/Vehicle'
+
+function App() {
+	return (
+		<>
+			<Header />
+			<Privelege />
+			<Price />
+			<Vehicle />
+			<Contact />
+			<Footer />
+		</>
+	)
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,93 @@
+import '../styles/styles.scss'
+import headerStyles from '../styles/header.module.scss'
+import { useState } from 'react'
+
+const Header = () => {
+	const [burgerOpen, setBurgerOpen] = useState(false)
+
+	return (
+		<section className={headerStyles.header}>
+			<div className={headerStyles.header_left}>
+				<div className={headerStyles.header_logo}>
+					<a href='/'>
+						<img src='images/Logo.png' alt='' />
+					</a>
+				</div>
+				<div className={headerStyles.header_title}>
+					<div className={headerStyles.header_title_name}>
+						<a href='/'>ALEXBETON</a>
+					</div>
+					<div className={headerStyles.header_description}>
+						<a href='/'>Производство и продажа строительных материалов</a>
+					</div>
+				</div>
+			</div>
+			<div
+				className={
+					!burgerOpen
+						? headerStyles.header_menu_icon
+						: headerStyles.header_menu_icon_active
+				}
+				onClick={() => {
+					setBurgerOpen(prevState => !prevState)
+				}}
+			>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<nav
+				className={
+					!burgerOpen
+						? headerStyles.header_right
+						: headerStyles.header_right_active
+				}
+			>
+				<ul className={headerStyles.header_navigation_list}>
+					<li>
+						<a
+							href='#privelege'
+							onClick={() => {
+								setBurgerOpen(prevState => !prevState)
+							}}
+						>
+							ГЛАВНАЯ
+						</a>
+					</li>
+					<li>
+						<a
+							href='#price'
+							onClick={() => {
+								setBurgerOpen(prevState => !prevState)
+							}}
+						>
+							ТОВАР
+						</a>
+					</li>
+					<li>
+						<a
+							href='#vehicle'
+							onClick={() => {
+								setBurgerOpen(prevState => !prevState)
+							}}
+						>
+							ТЕХНИКА
+						</a>
+					</li>
+					<li>
+						<a
+							href='#contacts'
+							onClick={() => {
+								setBurgerOpen(prevState => !prevState)
+							}}
+						>
+							КОНТАКТЫ
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</section>
+	)
+}
+
+export default Header
